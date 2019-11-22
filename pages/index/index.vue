1fb5374b076b75422e2979fb4e3c706b204b5a20
Leon Song <leon.ting@okkuaixiu.com>
2019-11-22 15:15:52 +0800

    xxx

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,13 @@
 <template>
 	<view class="user-box">
-		<input class="sign" type="text" v-model="name" />
-		<view class="sendMsg" @click="sendSocketMessage">
-			签到
+		<view class="send-box">
+			<input class="sign" type="text" v-model="name" />
+			<view class="sendMsg" @click="sendSocketMessage">
+				签到
+			</view>
+		</view>
+		<view class="user-bubble" v-if="up" :style="{'animation-play-state':paused}">
+			<view class="uname">{{name}}</view>
 		</view>
 	</view>
 </template>
@@ -11,7 +16,9 @@
 	export default {
 		data() {
 			return {
-				name: ""
+				name: "",
+				up: false,
+				paused: "paused"
 			}
 		},
 		onLoad() {
@@ -24,9 +31,17 @@
 		methods: {
 			sendSocketMessage() {
 				var that = this;
+				that.up = true;
+				that.paused = "running";
 				let _data = {
 					"msg": that.name
 				};
+				_data["fun"] = function() {
+					setTimeout(() => {
+						that.up = false;
+						that.paused = "paused";
+					}, 3000)
+				}
 				console.log(_data);
 				that.$store.dispatch("sendSocketMessage", _data)
 			}
@@ -35,8 +50,20 @@
 </script>
 
 <style>
+	page {
+		height: 100%;
+		width: 100%;
+		background: #000;
+	}
+
 	.user-box {
+		position: relative;
 		padding: 20upx;
+		height: 100%;
+	}
+
+	.send-box {
+		z-index: 3;
 	}
 
 	.sendMsg {
@@ -54,10 +81,90 @@
 	}
 
 	.sign {
+		background: #FFFFFF;
 		line-height: 2;
 		font-size: 48upx;
 		border: 2upx solid #CCCCCC;
 		padding: 10upx 20upx;
 		border-radius: 25upx;
+	}
+
+	.user-bubble {
+		position: absolute;
+		width: 100%;
+		bottom: 50%;
+		opacity: 0;
+		z-index: 1;
+		animation-name: fadeUpOut;
+		animation-duration: 3s;
+		animation-timing-function: linear;
+		animation-iteration-count:infinite;
+		animation-fill-mode: none;
+		/* forwards; */
+	}
+
+	@keyframes fadeUpOut {
+		0% {
+			bottom: 50%;
+			opacity: 1;
+		}
+
+		5% {
+			opacity: 0.5;
+		}
+
+		10% {
+			opacity: 0.8;
+		}
+
+		30% {
+			opacity: 1;
+		}
+
+		100% {
+			opacity: 1;
+			bottom: 120%;
+		}
+	}
+
+	.uname {
+		color: #FFF;
+		font-size: 50upx;
+		line-height: 1;
+		display: flex;
+		justify-content: center;
+		flex-direction: row;
+		align-content: center;
+		align-items: center;
+
+		animation-name: flash;
+		animation-duration: 1.5s;
+		animation-timing-function: ease-in-out;
+		animation-iteration-count: infinite;
+		animation-direction: alternate;
+	}
+
+	@keyframes flash {
+		from {
+			text-shadow: 0 0 10px #fff,
+				0 0 20px #fff,
+				0 0 30px #fff,
+				0 0 40px #228DFF,
+				0 0 70px #228DFF,
+				0 0 80px #228DFF,
+				0 0 100px #228DFF,
+				0 0 150px #228DFF;
+		}
+
+		to {
+			text-shadow: 0 0 5px #fff,
+				0 0 10px #fff,
+				0 0 15px #fff,
+				0 0 20px #228DFF,
+				0 0 35px #228DFF,
+				0 0 40px #228DFF,
+				0 0 50px #228DFF,
+				0 0 75px #228DFF;
+		}
 	}
 </style>
